3f28f63f280b601ab9fba35f94b93343b188baca
don1997 <donald_j97@hotmail.com>
2021-12-16 16:31:59 -0500

    finished one lesson

--- tic_tac_toe/lessons/2dArray.c
+++ tic_tac_toe/lessons/2dArray.c
@@ -5,21 +5,26 @@
 //for 1d arrays was simpler and the approach has some extra steps
 //Luckily I managed to find a good resource to learn it
 //NEXT STEPS: Next steps are to update a 2D array and the implement the core functions into the actual program.
-
+//FINAL SUMMARY: I have managed to complete adding an UPDATE functions. I will keep this copy as reference since it works. For the next copy I will 
+//convert the arrays to char since tic tac toe requires x's and o's in order to play the game correctly.
 #include<stdio.h>
 
 const int M = 3;
  
- 
 void print(int (*arr)[M])
 {
     int i, j;
-    for (i = 0; i < M; i++)
-    for (j = 0; j < M; j++)
-        printf("%d ", arr[i][j]);
+    for (i = 0; i < M; i++){
+    	for (j = 0; j < M; j++){
+     
+		printf("%d ", arr[i][j]);
+    	}
+	printf("\n");
+    }
+printf("\n");
 }
  
-void SEARCH(int(*arr)[M], int in){
+int SEARCH(int(*arr)[M], int in){
 
 	int i, j;
 
@@ -28,7 +33,7 @@ void SEARCH(int(*arr)[M], int in){
 		for(j= 0; j<M;j++){
 		
 			if(in == arr[i][j]){
-				
+				return 1;
 				printf(" FOUND");
 
 			}
@@ -36,17 +41,46 @@ void SEARCH(int(*arr)[M], int in){
 		}
 
 	}
-
+	return 0;
 }
+
+void UPDATE(int(*arr)[M], int in, int in2){
+
+        if(SEARCH(arr, in) == 1){
+
+
+       		 arr[in][in2] = 101; 
+        }
+	else{
+
+		printf("Not found");
+	}
+}
+ 
+
 int main()
 {
-    int userIN;
-    int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    print(arr);
+    int userIN,userIN2;
+    int arr[][3] = {{0, 1, 2}, {3, 4, 5}, {6 ,7 , 8}};
     
-    printf("select: ");
+    print(arr);
+	
+
+    printf("\n");    
+    
+    printf("select row: ");
     scanf("%d", &userIN);
-    SEARCH(arr, userIN);
+
+    printf("select Column: ");
+    scanf("%d", &userIN2);
+
+    UPDATE(arr, userIN, userIN2);
+    
+    print(arr);
+
+    printf("\n");
+
+
     return 0;
 }
 
